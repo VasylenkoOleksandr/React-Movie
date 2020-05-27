279fdeab1e542d859ba529542e3e9cecab9c4729
VasylenkoOleksandr <58167101+VasylenkoOleksandr@users.noreply.github.com>
2020-05-27 22:24:32 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Movie
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+function MovieItem() {
+  return (
+    <div>
+      <p>Title</p>
+      <p>8.0</p>
+    </div>
+  );
+}
+export default function App() {
+  return (
+    <div>
+      <MovieItem />
+    </div>
+  );
+}
